76e5f2b340a7a15b7097e7a22441fe20a8c07170
chinmay <164729117+ChinmayLokahnde@users.noreply.github.com>
2025-01-16 12:30:49 +0530

    Delete src directory

--- src/login/loginDB.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema, model } from "mongoose";
-
-mongoose.connect('mongodb+srv://chinmaylokhande30:chinmay123@cluster0.pw4pyt2.mongodb.net/login')
-const userSchema = new  Schema({
-    username:String,
-    password:String
-});
-
-export default model('User', userSchema);
